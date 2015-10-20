9d04151062b144f6ac1de8d38091e9e669737348
Drewster317 <drewster317@gmail.com>
2015-10-20 09:39:35 -0400

    assessmnet

--- challenge.rb
+++ challenge.rb
@@ -4,31 +4,45 @@
 # Question 1: Ruby objects and classes.
 # How would one find the class of the below objects.
 
-"Hello World"
-33
-65.89
+"Hello World".class
+33.class
+65.89.class
 
 # Question 2: Booleans
 # What would be the result of executing the below statements
 
-if 33 && 55
+if "hello" && 4.5
   puts "it's true"
 else
   puts "it's false"
 end
 
+"it's true" would be the result.
+
 # 2.1 Substitue "hello" && 4.5 for 33 && 55 above. What's the output?
+
+"it's true"
 # 2.2 Substitue nil && 88 for 33 && 55 above. What's the output?
+
+"it's false"
 # 2.4 Substitue 63.4 < 64. What's the output?
+
+"it's true"
 
 # Question 3 :Strings
 
 # How would you (Yes, you can look up the String methods!):
 
 # 3.1. determine if a string is included another another string?
+
 # 3.2. determine if the length of a string?
+
+"string".length
+
 # 3.3. Go from the string "eels in my hovercraft" to "fish in my sink" but don't
-# change the string "eels in my hovercraft"
+#change the string "eels in my hovercraft"
+
+use +=
 
 
 # Question 4 : Variables
@@ -37,10 +51,18 @@ end
 
 vehicle = "4 wheel Bus"
 
+
 # 4.1 to "6 wheel Bus"
+
+vehicle = "6 wheel Bus"
+
 # 4.2 to "6 wheel tank"
+vehicle = "6 wheel tank"
+
 # 4.3 Create a NEW string from the string in 4.2 that will be
 # "6 wheel tank that shoots rainbow bunnnies"
+
+vehicle << "that shoots rainbow bunnies"
 
 # Question 5 : Loop
 # - write a loop that guesses your age.
